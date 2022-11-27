32670a9eaf6975b930dbd1ead08a174f60ddd29f
madalina.toma <madalina.toma@softem.ro>
2022-11-27 21:36:31 +0200

    Add project files.

---
+++ lab1ex9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab1ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Scrieti un program care interschimba valoarea a doua variabile intregi.
+             */
+
+            int a;
+            int b;
+
+            Console.WriteLine("Introduceti primul numar: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti al doilea: ");
+            b = int.Parse(Console.ReadLine());
+            (a, b) = (b, a);
+           
+            Console.WriteLine("a={0} b={1}",a,b );
+        }
+    }
+}
